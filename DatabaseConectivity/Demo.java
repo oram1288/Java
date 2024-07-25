c92d56186d1e54f2d301a1116a12e5d82f6cc4e9
oram1288 <cody.oram@keyin.com>
2024-07-25 13:14:50 -0230

    july25

--- DatabaseConectivity/Demo.java
+++ DatabaseConectivity/Demo.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Demo {
+public class Demo{
     public static void main(String[] args) {
          WriteDB();
          ReadDB();
---
+++ Week14_GUI/ShowColors.java
@@ -0,0 +1,87 @@
+package Week14_GUI;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.text.Normalizer.Form;
+import javax.swing.JFrame;
+import javax.swing.JTextField;  
+
+public class ShowColors extends Frame implements ActionListener {
+   final TextField  tu, tpw;  
+   public ShowColors() {
+  
+      // creating a Frame  
+      Frame f = new Frame();  
+      f.setSize(400,400);  
+      f.setTitle("Employee Login");   
+      f.setLayout(null);  
+
+      Label lu = new Label("Username");   
+      tu = new TextField(); 
+      Label lpw = new Label("Password");   
+      tpw = new TextField();  
+      Button b = new Button("Submit");  
+  
+      // setting position of above components in the frame  
+      lu.setBounds(20, 80, 80, 30);  
+      tu.setBounds(20, 110, 180, 30);  
+      lpw.setBounds(20, 130, 80, 30);  
+      tpw.setBounds(20, 160, 180, 30);  
+      b.setBounds(100, 200, 80, 30);  
+      // adding components into frame    
+      f.add(b);  
+      f.add(lu);  
+      f.add(tu);  
+      f.add(lpw);  
+      f.add(tpw); 
+         b.addActionListener(this);  
+      
+      setSize(400,400);
+      // setting visibility of frame  
+      f.setVisible(true);  
+   
+   }
+    public void actionPerformed(ActionEvent e){  
+    String UN = tu.getText();
+    String PW = tpw.getText();
+    if (UN.equals("a@b.com") && PW.equals("test")) {  //if authentic, navigate user to a new page  
+                
+    //create instance of the NewPage  
+    Frame page = new Frame();  
+        page.setSize(400,400);
+    //make page visible to the user  
+    page.setVisible(true);  
+        
+    //create a welcome label and set it to the new page  
+    Label wel_label = new Label("Welcome: "+UN);  
+    page.add(wel_label);
+    }  
+    else{  
+    //show error message  
+    System.out.println("Please enter valid username and password");  
+    }  
+        
+    }  
+
+    public void paint( Graphics g ) {
+    Color c = Color.yellow;
+        g.setColor( c );
+        g.fillOval(25, 50, 100, 100);
+        c = Color.black;
+        g.setColor( c );
+        g.drawOval(50, 70, 20, 25);
+        g.drawOval(80, 70, 20, 25);
+        g.drawArc(40, 70, 70,60,0,-180);
+        g.drawArc(41, 70, 70,60,0,-180);
+        g.drawArc(42, 70, 70,60,0,-180);
+
+
+        g.setColor( new Color( 255, 0, 0 ) );
+        g.drawString( "Current RGB: " + g.getColor(), 150, 65 );
+        // set new drawing color using floats
+    }   
+    public static void main( String args[] )  {
+    ShowColors app = new ShowColors();
+
+    }
+}
